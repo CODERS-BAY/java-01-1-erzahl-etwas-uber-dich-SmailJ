5493b4b1469028055034ed9cd1b56e11d714ca70
Smail Jusic <smail1997@yahoo.de>
2020-08-26 17:24:11 +0200

    Finish

---
+++ Aufgabe1.1/src/Aufgabe/Main.java
@@ -0,0 +1,23 @@
+package Aufgabe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age=22;
+        String firstName="Smail";
+        char gender='M';
+        String lastName="Jusic";
+        String bDay="05.09.1997";
+        double grade=2.7;
+        boolean married=false;
+
+        System.out.println(age);
+        System.out.println(firstName);
+        System.out.println(gender);
+        System.out.println(lastName);
+        System.out.println(bDay);
+        System.out.println(grade);
+        System.out.println(married);
+    }
+}
